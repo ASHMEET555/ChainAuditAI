20d7104e22f051a328f46316eb3d96e580c003a3
Reaper-ai <gauravupreti232+github@gmail.com>
2026-02-07 19:46:48 +0530

    load models

---
+++ backend/load_models.py
@@ -0,0 +1,35 @@
+import joblib
+
+def load_model_vehicle():
+    model_path = 'model_wts/vehicle_model_weights.pkl'
+    features_path = 'model_wts/vehicle_model_features.pkl'
+
+    model = joblib.load(model_path)
+    features = joblib.load(features_path)
+
+    return model, features
+
+def load_model_bank():
+    model_path = 'model_wts/bank_model_weights.pkl'
+    features_path = 'model_wts/bank_model_features.pkl'
+
+    model = joblib.load(model_path)
+    features = joblib.load(features_path)
+
+def load_model_ecommerce():
+    model_path = 'model_wts/ecommerce_model_weights.pkl'
+    features_path = 'model_wts/ecommerce_model_features.pkl'
+
+    model = joblib.load(model_path)
+    features = joblib.load(features_path)
+
+    return model, features
+
+def load_model_ethernet():
+    model_path = 'model_wts/ethernet_model_weights.pkl'
+    features_path = 'model_wts/ethernet_model_features.pkl'
+
+    model = joblib.load(model_path)
+    features = joblib.load(features_path)
+
+    return model, features
